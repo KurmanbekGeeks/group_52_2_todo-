2d1d69f03925d6c6980d6d425e6bf83a3306d83a
kurmanbek <kmomunkulov@crm.kg>
2025-04-21 20:45:04 +0600

    Lesson 6

--- main.py
+++ main.py
@@ -4,9 +4,10 @@ from db import main_db
 
 def main(page: ft.Page):
     page.title = 'ToDo List'
-    page.padding = 40
-    page.bg_color = ft.colors.GREY_600
-    page.theme_mode = ft.ThemeMode.LIGHT
+    # page.padding = 40
+    # page.bg_color = ft.colors.GREY_600
+    page.theme_mode = ft.ThemeMode.DARK
+    page.window_maximized = True
     
 
     task_list = ft.Column(spacing=10)
@@ -33,7 +34,8 @@ def main(page: ft.Page):
         return ft.Row([
             task_field,
             ft.IconButton(ft.icons.EDIT, icon_color=ft.colors.YELLOW_400, on_click=enable_edit),
-            ft.IconButton(ft.icons.SAVE, icon_color=ft.colors.GREEN_400, on_click=save_edit)
+            ft.IconButton(ft.icons.SAVE, icon_color=ft.colors.GREEN_400, on_click=save_edit),
+            ft.IconButton(ft.icons.DELETE, icon_color=ft.colors.RED_400, on_click=lambda e: delete_task(task_id))
         ], alignment=ft.MainAxisAlignment.SPACE_BETWEEN)
 
     def add_task(e):
@@ -43,16 +45,43 @@ def main(page: ft.Page):
             task_input.value = ""
             page.update()
 
+    def delete_task(task_id):
+        main_db.delete_task_db(task_id)
+        load_tasks()
+
     task_input = ft.TextField(hint_text="Добавьте задачу", expand=True, dense=True, on_submit=add_task)
 
     add_button = ft.ElevatedButton("Добавить", on_click=add_task, icon=ft.icons.ADD, icon_color=ft.colors.GREEN_400)
 
-    page.add(
-        ft.Column([
-            ft.Row([task_input, add_button], alignment=ft.MainAxisAlignment.SPACE_BETWEEN),
-            task_list
-        ])
+    # page.add(
+    #     ft.Column([
+    #         ft.Row([task_input, add_button], alignment=ft.MainAxisAlignment.SPACE_BETWEEN),
+    #         task_list
+    #     ])
+    # )
+
+    content = ft.Container(content = ft.Column([
+        ft.Row([task_input, add_button], alignment=ft.MainAxisAlignment.SPACE_BETWEEN),
+        task_list
+    ], alignment=ft.MainAxisAlignment.CENTER),
+    padding=20, alignment=ft.alignment.center)
+
+    background_image = ft.Image(
+        src='/Users/kurmanbek/Desktop/Geeks/Groups_flet/group_52_2_to_do_list/image.png',
+        fit=ft.ImageFit.FILL,
+        width=page.width,
+        height=page.height
     )
+
+    background = ft.Stack([background_image, content])
+
+    def on_resize(e):
+        background_image.width = page.width
+        background_image.height = page.height
+        background.update()
+
+    page.add(background)
+    page.on_resize = on_resize
 
     load_tasks()
 
